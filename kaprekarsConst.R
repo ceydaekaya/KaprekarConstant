f5da22746d37a129efcdddd6fa3f25a541cde14f
ceyda <64362304+ceydaekaya@users.noreply.github.com>
2022-02-09 11:52:20 -0600

    num update

--- kaprekarsConst.R
+++ kaprekarsConst.R
@@ -7,11 +7,14 @@ kaprekar_test <- function(num){
     while (num != kaprekarConst) {
       count<-count+1
       nums <- as.integer(str_extract_all(num, "[0-9]")[[1]])
+      
       Dec <- as.integer(str_sort(nums, decreasing = TRUE))
       Dec <- as.integer(paste(Dec, collapse = ""))
+      
       Inc <- as.integer(str_sort(nums, decreasing = FALSE))
       Inc <- as.integer(paste(Inc, collapse = ""))
-      num = as.integer(Dec) - as.integer(Inc)
+      new_num = as.integer(Dec) - as.integer(Inc)
+      num<-new_num
       print(paste0('Iteration ',count,':  ', as.integer(Dec), ' ', as.integer(Inc), ' => ', as.integer(num)))
         }
   } 
